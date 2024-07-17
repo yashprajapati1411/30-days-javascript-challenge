278787fa8eb860549ecf0a6759a0e10d8c02e3c2
Yash Prajapati <138312580+yashprajapati1411@users.noreply.github.com>
2024-07-17 19:39:05 +0000

    variable and datatype

--- varaible_and_datatypes/index.js
+++ varaible_and_datatypes/index.js
@@ -1,1 +1,37 @@
-console.log("yash")
+// declare a variable
+
+let a=5
+// console.log(a)
+
+var b=10;
+// console.log(b)
+
+//task 2: Declare a variable using let, assign it a string and log the value to the console
+
+let name="yash"
+// console.log(name)
+
+//**********activity 2 */
+//declare variable using const assign it a boolean value and log the value to console
+
+const isloggedin =true;
+console.log(isloggedin)
+
+//activity 3:Data types
+
+//task 4:  create variable of different data types and log each variable's tyope using the typeof operator
+
+let marks=100
+let surname="prajapati"
+let isloggediin=true
+let stud_name ={
+    surname:"prajapati",
+    fname:"yash"
+};
+const myArr=["shaktiman","tarzan","bhim"]
+
+console.log(typeof(marks))
+console.log(typeof(surname))
+console.log(typeof(isloggediin))
+console.log(typeof(stud_name))
+console.log(typeof(myArr))
